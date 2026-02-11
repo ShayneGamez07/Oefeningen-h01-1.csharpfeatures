83d1343ffe230fe208d677c0dea884322bd36868
ShayneGamez <Shayne.Vanoutryve@student.howest.be>
2026-02-11 11:06:50 +0100

    NullMovies.App -klaar-

--- H01.NullMovies.App/Program.cs
+++ H01.NullMovies.App/Program.cs
@@ -1,1 +1,41 @@
-﻿);
+﻿using System;
+using System.Collections.Generic;
+using H01.NullMovies.Core.Models;
+
+namespace H01.NullMovies.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = Movie.GetMovies();
+            var output = new List<string>();
+
+            foreach (var movie in movies)
+            {
+                output.Add(BuildMovieString(movie));
+            }
+
+            PrintMovies(output);
+        }
+
+        private static string BuildMovieString(Movie movie)
+        {
+            return $"""
+            Id: {movie.Id ?? -1}
+            Title: {movie.Title ?? "Geen titel"}
+            Director: {movie.Director ?? "Onbekend"}
+            Sequel: {movie.Sequel?.Title ?? "Geen sequel"}
+            """;
+        }
+
+        private static void PrintMovies(List<string> movies)
+        {
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie);
+                Console.WriteLine("--------------");
+            }
+        }
+    }
+}
