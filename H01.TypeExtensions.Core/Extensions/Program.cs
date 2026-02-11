d373815924b0b46b95a0517b5231a344f2b802cc
ShayneGamez <Shayne.Vanoutryve@student.howest.be>
2026-02-11 11:36:58 +0100

    H01.ExtensionMethods.App -klaar-

---
+++ H01.TypeExtensions.Core/Extensions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using H01.TypeExtensions.Core.Extensions;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 6;
+        Console.WriteLine($"{x}² = {x.PowerOfTwo()}");
+
+        string text = "Hallo Howest Web Backend";
+        Console.WriteLine($"Chars: {text.CountChars()}");
+        Console.WriteLine($"Spaces: {text.CountSpaces()}");
+        Console.WriteLine($"'Web' count: {text.CountSubstring("Web")}");
+
+        var numbers = new List<int> { 6, 3, 0, 6 };
+        Console.WriteLine($"RMS = {numbers.SquareMeanRoot()}");
+    }
+}
---
+++ H01.TypeExtensions.Core/Extensions/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace H01.TypeExtensions.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int CountChars(this string text)
+            => text.Length;
+
+        public static int CountSpaces(this string text)
+            => text.Count(c => c == ' ');
+
+        public static int CountSubstring(this string text, string search)
+            => text.Split(search).Length - 1;
+    }
+}
