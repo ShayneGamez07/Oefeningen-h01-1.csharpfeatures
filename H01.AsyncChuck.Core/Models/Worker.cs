8e84616551994ad2c6bf5ff5cb88ea2b9e870061
ShayneGamez <Shayne.Vanoutryve@student.howest.be>
2026-02-11 11:29:33 +0100

    H01.AsyncChuck.App -klaar-

---
+++ H01.AsyncChuck.Core/Models/Worker.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using H01.AsyncChuck.Core.Models;
+
+public class Worker
+{
+    public async Task DoWork()
+    {
+        string numbersOutput = await GenerateNumbersAsync();
+        Console.WriteLine(numbersOutput);
+
+        var quote = await GetNorrisQuote();
+        Console.WriteLine($"Chuck says: {quote.Value}");
+    }
+
+    public Task<string> GenerateNumbersAsync()
+    {
+        return Task.Run(() => GenerateNumbers());
+    }
+
+    private string GenerateNumbers()
+    {
+        return "Numbers generated!";
+    }
+
+    public async Task<NorrisQuoteDto> GetNorrisQuote()
+    {
+        string ApiUrl = "https://api.chucknorris.io/jokes/random";
+        var client = new HttpClient();
+
+        var response = await client.GetAsync(ApiUrl);
+        var content = await response.Content.ReadAsStringAsync();
+
+        return JsonConvert.DeserializeObject<NorrisQuoteDto>(content);
+    }
+}
